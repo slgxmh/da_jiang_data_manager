ee126465e3c1bae826568c2a84e2df54047709b3
slgxmh <slgxmh@foxmail.com>
2025-08-25 19:40:09 +0800

    feat: vis mrk items.

--- lib/widgets/app_map.dart
+++ lib/widgets/app_map.dart
@@ -2,6 +2,7 @@ import 'package:da_jiang_data_manager/controller/workspace.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:get/get.dart';
+import 'package:latlong2/latlong.dart';
 
 class AppMap extends StatefulWidget {
   const AppMap({super.key});
@@ -13,6 +14,25 @@ class AppMap extends StatefulWidget {
 class _AppMapState extends State<AppMap> {
   final _mapController = MapController();
   bool _isMapReady = false;
+
+  List<Marker> _buildMarkers(WorkspaceController controller) {
+    final markers = <Marker>[];
+    for (final mrkData in controller.mrkDatas) {
+      for (final item in mrkData.items) {
+        markers.add(
+          Marker(
+            point: LatLng(item.lat, item.lon),
+            child: Icon(
+              Icons.location_on,
+              color: Colors.red,
+              key: Key('mrk_item_${mrkData.basePath}_${item.id}'),
+            ),
+          ),
+        );
+      }
+    }
+    return markers;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,6 +60,9 @@ class _AppMapState extends State<AppMap> {
                   'https://wprd0{s}.is.autonavi.com/appmaptile?lang=zh_cn&style=8&x={x}&y={y}&z={z}',
               subdomains: const ['1', '2', '3', '4'],
             ),
+            MarkerLayer(
+              markers: _buildMarkers(controller),
+            )
           ],
         );
       },
